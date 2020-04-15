6382d656ec20a63b6aa5b6adc35b99672cbe0b78
MOREIL <caroline.moreil@epitech.eu>
2020-04-15 17:38:15 +0200

    V0 avec menu et base HTML à développer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpClient\HttpClient;
 
 class HomeController extends AbstractController
 {
@@ -9,4 +12,15 @@ class HomeController extends AbstractController
     {
         return $this->render('home/home.html.twig');
     }
+
+    public function Menu()
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'http://web.cryfter.ovh:1337/pizzas');
+        $pizzas = $response->toArray();
+
+        return $this->render('home/home.html.twig', [
+            'pizzas' => $pizzas
+        ]);
+   }
 }
