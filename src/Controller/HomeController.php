ab16d84c6bf2237a77a017c76c5c8df7a0dcc0b0
MOREIL <caroline.moreil@epitech.eu>
2020-04-17 11:31:38 +0200

    V je sais plus commbien mais 3 je crois

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,29 +2,72 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpClient\HttpClient;
 
 class HomeController extends AbstractController
 {
     public function index()
     {
-        return $this->render('home.html.twig');
+        return $this->render('home/home.html.twig');
     }
-    public function menu()
+
+    public function menu(SessionInterface $session)
     {
+        $pizzaToCart = $session->get('pizzaToCart', []);
+        $order = [];
+
+        //$session->clear();
+
         $client = HttpClient::create();
         $response = $client->request('GET', 'http://web.cryfter.ovh:1337/pizzas');
         $pizzas = $response->toArray();
 
-            // 'pizzas' => $pizzas
-        return $this->render('home/home.html.twig', ['pizzas' => $pizzas
+        foreach ($pizzaToCart as $idPizza => $quantity)
+        {
+            foreach ($pizzas as $pizza)
+            {
+                if($pizza['id'] == $idPizza)
+                {
+                    $order[] = [
+                        'pizza' => $pizza,
+                        'quantity' => $quantity
+                    ];
+                }
+            }
+        }
+
+        //dump($order);
+
+        return $this->render('home/home.html.twig', [
+            'pizzas' => $pizzas,
+            'order' => $order
         ]);
-   }
+    }
+
+   public function add ($id, SessionInterface $session)
+    {
+        $pizzaToCart = $session->get('pizzaToCart', []);
+
+        if(!empty($pizzaToCart[$id]))
+        {
+            $pizzaToCart[$id] ++;
+        }
+        else
+        {
+            $pizzaToCart[$id] = 1;
+        }
+
+        $session->set('pizzaToCart', $pizzaToCart);
+
+        return $this->redirectToRoute('menu');
+
+    }
 
 
-    public function buttonPressed()
+    /*public function buttonPressed()
     {
         if(isset($_POST['validateCart']))
             return $this->validateCart();
@@ -32,6 +75,6 @@ class HomeController extends AbstractController
 
     public function validateCart()
     {
-        
-    }
+
+    }*/
 }
